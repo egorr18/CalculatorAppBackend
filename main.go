269bdafec818d283e3ca26d4adf2f77960776c24
egorr18 <egorkakorol2006@gmail.com>
2025-04-12 13:13:36 +0300

    append Patch and Delete

--- main.go
+++ main.go
@@ -24,8 +24,8 @@ type CalculationRequest struct {
 // Глобальна перемінна історія наших виразів
 var calculations = []Calculation{}
 
-// calculationExpression обробляє математичний вираз і повертає результат як рядок
-func calculationExpression(expression string) (string, error) {
+// CalculationExpression обробляє математичний вираз і повертає результат як рядок
+func CalculationExpression(expression string) (string, error) {
 	expr, err := govaluate.NewEvaluableExpression(expression) // Створили вираз (55+55) // Дозволяє працювати з більш гнучкими та складними виразами, наприклад, з кастомними функціями.
 	if err != nil {
 		return "", err // Передали 55++55
@@ -38,21 +38,22 @@ func calculationExpression(expression string) (string, error) {
 	return fmt.Sprintf("%v", result), nil
 }
 
-// getCalculations повертає всі обчислення у форматі JSON
-func getCalculations(c echo.Context) error {
+// GetCalculations повертає всі обчислення у форматі JSON
+func GetCalculations(c echo.Context) error {
 	return c.JSON(http.StatusOK, calculations)
 }
 
-// Передає JSON з expression
-func postCalculations(c echo.Context) error {
+// PostCalculations Передає JSON з expression
+func PostCalculations(c echo.Context) error {
+	//Передаємо запрос
 	var req CalculationRequest
 	//«Декодуємо запит» — це означає «читаємо дані, які прийшли від клієнта, і перетворюємо їх у зручну структуру, з якою можна працювати в коді».
 	if err := c.Bind(&req); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
 	}
 
-	// рахуємо
-	result, err := calculationExpression(req.Expression)
+	// Рахуємо новий результат
+	result, err := CalculationExpression(req.Expression)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"}) // map — структура для зберігання даних у форматі "ключ → значення"
 	} // Якщо все ок
@@ -70,7 +71,7 @@ func postCalculations(c echo.Context) error {
 }
 
 func PatchCalculations(c echo.Context) error {
-	//достає id з силки яку ми хочемо обновити
+	//Дістає id з силки яку ми хочемо обновити
 	id := c.Param("id")
 
 	//Передаємо запрос
@@ -79,17 +80,31 @@ func PatchCalculations(c echo.Context) error {
 	if err := c.Bind(&req); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
 	}
-	// рахуємо новий результат
+	// Рахуємо новий результат
 	result, err := CalculationExpression(req.Expression)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
 	}
 
+	//Оновлюємо
 	for i, calculation := range calculations {
 		if calculation.ID == id {
 			calculations[i].Expression = req.Expression
 			calculations[i].Result = result
 			return c.JSON(http.StatusOK, calculations[i])
+		}
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Calculation not found"})
+}
+
+func DeleteCalculations(c echo.Context) error {
+	id := c.Param("id")
+
+	for i, calculation := range calculations {
+		if calculation.ID == id {
+			calculations = append(calculations[:i], calculations[i+1:]...)
+			//Повертає відповідь без тіла 'код 204'
+			return c.NoContent(http.StatusNoContent)
 		}
 	}
 	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Calculation not found"})
@@ -101,8 +116,10 @@ func main() {
 	e.Use(middleware.CORS())   // запит -> middleware -> обробка CORS -> передає на сервер
 	e.Use(middleware.Logger()) // запрос -> middleware -> логірує -> передає на сервер
 
-	e.GET("/calculations", getCalculations)
-	e.POST("/calculations", postCalculations)
+	e.GET("/calculations", GetCalculations)
+	e.POST("/calculations", PostCalculations)
+	e.PATCH("/calculations/:id", PatchCalculations)
+	e.DELETE("/calculations/:id", DeleteCalculations)
 
 	e.Start("localhost:8080")
 }
